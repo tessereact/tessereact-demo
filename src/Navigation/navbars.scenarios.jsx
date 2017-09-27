998c71bedb1bf43323d5f0f7e591a0cad0f62d39
Lesha Koss <regiusprod@gmail.com>
2017-09-27 11:19:23 +0600

    Add Navigation scenarios

---
+++ src/Navigation/navbars.scenarios.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import { context, scenario as scenarioFn } from 'tessereact'
+import { Navbar, Nav, NavItem, NavDropdown, FormGroup, FormControl, MenuItem, Button } from 'react-bootstrap'
+
+const scenario = (name, fn) => scenarioFn(name, fn, {css: true, screenshot: true})
+
+context('Navigation: Navbars', () => {
+  scenario('Basic example', () =>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">React-Bootstrap</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+    </Navbar>
+  )
+
+  scenario('Responsive', () =>
+    <Navbar inverse collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">React-Bootstrap</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="#">Link</NavItem>
+          <NavItem eventKey={2} href="#">Link</NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">Link Right</NavItem>
+          <NavItem eventKey={2} href="#">Link Right</NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+
+  scenario('Forms', () =>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">Brand</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Navbar.Form pullLeft>
+          <FormGroup>
+            <FormControl type="text" placeholder="Search" />
+          </FormGroup>
+          {' '}
+          <Button type="submit">Submit</Button>
+        </Navbar.Form>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+
+  scenario('Text and Non-nav links', () =>
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">Brand</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Navbar.Text>
+          Signed in as: <Navbar.Link href="#">Mark Otto</Navbar.Link>
+        </Navbar.Text>
+        <Navbar.Text pullRight>
+          Have a great day!
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+
+
+})
---
+++ src/Navigation/pagination.scenarios.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { context, scenario as scenarioFn } from 'tessereact'
+import { Pagination } from 'react-bootstrap'
+
+const scenario = (name, fn) => scenarioFn(name, fn, {css: true, screenshot: true})
+
+context('Navigation: Pagination', () => {
+  scenario('Basic example', class PaginationBasic extends React.Component {
+    constructor (props, context) {
+      super(props, context)
+      this.state = {
+        activePage: 1
+      }
+    }
+
+    handleSelect = (eventKey) => {
+      this.setState({
+        activePage: eventKey
+      })
+    }
+
+    render() {
+      return (
+        <div>
+          <Pagination
+            bsSize="large"
+            items={10}
+            activePage={this.state.activePage}
+            onSelect={this.handleSelect} />
+          <br />
+
+          <Pagination
+            bsSize="medium"
+            items={10}
+            activePage={this.state.activePage}
+            onSelect={this.handleSelect} />
+          <br />
+
+          <Pagination
+            bsSize="small"
+            items={10}
+            activePage={this.state.activePage}
+            onSelect={this.handleSelect} />
+        </div>
+      )
+    }
+  })
+
+  scenario('More options', class PaginationAdvanced extends React.Component {
+    constructor (props, context) {
+      super(props, context)
+      this.state = {
+        activePage: 1
+      }
+    }
+
+    handleSelect = (eventKey) => {
+      this.setState({
+        activePage: eventKey
+      })
+    }
+
+    render() {
+      return (
+        <Pagination
+          prev
+          next
+          first
+          last
+          ellipsis
+          boundaryLinks
+          items={20}
+          maxButtons={5}
+          activePage={this.state.activePage}
+          onSelect={this.handleSelect} />
+      )
+    }
+  })
+})
